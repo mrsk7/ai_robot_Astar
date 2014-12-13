db9182310e9b1aa469fbcbae45dfe211ac2ddc4e
mrsk7 <mirozis@hotmail.com>
2014-12-14 01:29:59 +0200

    Working on ideas

--- Robots.java
+++ Robots.java
@@ -5,10 +5,69 @@ import java.util.PriorityQueue;
 import java.math.*;
 
 public class Robots {
-        
-        public static class Node{
+           
+
+        public static class Coords {
+            int x,y;
+            
+            public Coords(int a,int b) {
+                x=a;
+                y=b;
+            }
+
+            //This method overrides Object.equals()
+            public boolean equals(Object obj) {
+                    if(obj instanceof Coords) {
+                            Coords toCompare = (Coords) obj;
+                            return ((x == toCompare.x) && (y == toCompare.y));
+                    }
+                    return false;
+            }
+            //This method overrides Object.hashCode()
+            public int hashCode() {
+                int hash = 1;
+                hash = hash * 31 + x.hashCode();
+                hash = hash * 31 + y.hashCode();
+                return hash;
+            }
+        }
+
+        public static class iState{
+            public Coords robot1_coords,robot2,coords;
+
+            public iState(Node a, Node b) {
+                robot1_coords = new Coords(a.x,a.y);
+                robot2_coords = new Coords(b.x,b.y);
+            }
+
+            //This method overrides Object.equals()
+            public boolean equals(Object obj) {
+                    if(obj instanceof iState) {
+                            iState toCompare = (iState) obj;
+                            return ((robot1_coords.equals(toCompare.robot1_coords))&&(robot2_coords.equals(toCompare.robot2_coords)));
+                    }
+                    return false;
+            }
+
+            //This method overrides Object.hashCode()
+            public int hashCode() {
+                int hash = 1;
+                hash = hash * 31 + robot1_coords.hashCode();
+                hash = hash * 31 + robot2_coords.hashCode();
+                hash = hash * 31 + buf;
+                return hash;
+            }
+            
+            //TODO 
+            public boolean isGoal(String goal) {
+                    if (stack_two.equals(goal)) return true;
+                    else return false;
+            }
+        }
+        public static class Node implements Comparable<Node>{
             public int x,y;
-            public double g,h;
+            public double g = Double.POSITIVE_INFINITY;
+            public double h;
             public double f_x;
 
             public Node(int x_coord, int y_coord) {
@@ -18,6 +77,10 @@ public class Robots {
 
             public renew_cost() {
                 f_x = g+h;
+            }
+
+            public int compareTo(Node other) {
+                return Double.compare(f_x,other.f_x);
             }
 
         }
@@ -32,7 +95,7 @@ public class Robots {
                 //Eucleidian heuristic (admissible)
                 h = Math.sqrt(Math.pow(a.x-goal.x,2) + Math.pow(a.y-goal.y,2))
             }
-            else h=0;
+            else h=0;       //Djikstra
         }
 
         public static Node parseNode(BufferedReader reader) {
@@ -70,13 +133,53 @@ public class Robots {
             }
             return obstacles;
         }
-        public static void run(Node Robot1,Node Robot2,Node Goal,int[][] obs) {
+
+        public static LinkedList<Nodes> getNextNodes(Node curr,int[][] obs,N,M) {
+            //Four possible moves for every position, up, down, right or left
+            LinkdedList<Nodes> ll =  newLinkedList<Nodes>();
+            //if not at the top of the map
+            if (!(curr.y == 0))  {
+                Node up = new Node(curr.x,curr.y - 1);
+                ll.add(up);
+            }
+            //if not at the bottom of the map
+            if (!(curr.y == M-1)) {
+                Node down = new Node(curr.x,curr.y +1);
+                ll.add(down);
+            }
+            //if not at the right edge of the map
+            if (!(curr.x == N-1)) {
+                Node right = new Node(curr.x +1 ,curr.y);
+                ll.add(right);
+            }
+            //if not at the left edge of the map
+            if (!(curr.y == 0)) {
+                Node left = new Node(curr.x - 1,curr.y);
+                ll.add(left);
+            }
+            return ll;
+        }
+
+        public static void run(Node Robot1,Node Robot2,Node Goal,int[][] obs,N,M) {
             Robot1.g = Robot2.g = 0;
             Robot1.h = heuristic(Robot1,Goal,'M');
             Robot2.h = heuristic(Robot2,Goal,'M');
             Robot1.renew_cost();
             Robot2.renew_cost();
-            PriorityQueue<Node> q = new PriorityQueue<Node>();
+            PriorityQueue<Node> q1 = new PriorityQueue<Node>();
+            PriorityQueue<Node> q2 = new PriorityQueue<Node>();
+            q1.add(Robot1);
+            q2.add(Robot2);
+            Node current1,current2;
+            LinkdedList<Node> neighbours;
+            iState curState = new iState(Robot1,Robot2);
+            Hashtable<iState,iState> closed_set = new Hashtable<iState,iState>();
+            while ((!q1.isEmpty()) && (!q2.isEmpty())) {
+                current1 = q1.poll();
+                current2 = q2.poll();
+                neighbours=getNextNodes(current1,obs,N,M);
+                
+            }
         }
 
         /*public static double computePath(int start,BatStruct[] batArray,int spider_idx) {
@@ -142,6 +245,6 @@ public class Robots {
                 System.out.print(obstacles[i][j]);
             System.out.println();
         }*/
-        run(Robot1,Robot2,Goal,obstacles);
+        run(Robot1,Robot2,Goal,obstacles,N,M);
     }
 }
